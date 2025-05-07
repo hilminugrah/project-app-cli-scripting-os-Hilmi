a551b728f010f2949e1995fc162e377f69c3dd55
hilminugrah <hilminugrahs@gmail.com>
2025-05-07 14:04:46 +0700

    menambahkan fungsi untuk menampilkan menu

--- app.sh
+++ app.sh
@@ -10,3 +10,14 @@ NC='\033[0m' # No Color
 # Array untuk menyimpan daftar tugas
 declare -a tugas_list
 declare -a status_list
+
+# Fungsi menampilkan menu
+function show_menu() {
+  echo -e "${CYAN}===== Aplikasi Pengelola Tugas Mahasiswa =====${NC}"
+  echo "1. Tambah Tugas"
+  echo "2. Lihat Daftar Tugas"
+  echo "3. Hapus Tugas"
+  echo "4. Statistik Tugas"
+  echo "5. Keluar"
+  echo -n "Pilih menu [1-5]: "
+}
